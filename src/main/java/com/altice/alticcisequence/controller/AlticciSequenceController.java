903cf1a2eef547a5959db31cdfe909a5b887201c
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2022-09-30 02:25:48 +0100

    add cache-control

--- src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
+++ src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
@@ -1,6 +1,9 @@
 package com.altice.alticcisequence.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +26,12 @@ public class AlticciSequenceController {
 	@GetMapping("/{n}")
 	public ResponseEntity<Object> getAlticciSequenceValue(@PathVariable Long n) {
 
-		if(!this.alticciSequenceDto.isAlticciSequenceIndexValid(n)){
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("The input value must be zero or a natural number.");
-		} else {
+		if(this.alticciSequenceDto.isAlticciSequenceIndexValid(n)){
 			this.alticciSequenceDto.setAlticciSequenceIndex(n);
-			return ResponseEntity.status(HttpStatus.OK).body(
+			return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.maxAge(100, TimeUnit.SECONDS)).body(
 					this.alticciSequenceService.getAlticciSequenceValue(alticciSequenceDto.getAlticciSequenceIndex()));
+		} else {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("The input value must be zero or a natural number.");
 		}
 	}
 }
