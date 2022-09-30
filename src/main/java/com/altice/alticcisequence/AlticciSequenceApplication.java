b38513a0100d9a090c7c5b6f6a888d222b344929
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2022-09-30 08:02:48 +0100

    add openAPI-swagger documentation

--- src/main/java/com/altice/alticcisequence/AlticciSequenceApplication.java
+++ src/main/java/com/altice/alticcisequence/AlticciSequenceApplication.java
@@ -2,8 +2,10 @@ package com.altice.alticcisequence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class AlticciSequenceApplication {
 
 	public static void main(String[] args) {
