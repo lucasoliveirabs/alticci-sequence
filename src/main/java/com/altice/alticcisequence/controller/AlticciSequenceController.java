f656a7590624ed8dd531ba47dc8205b97eb1551d
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2022-10-04 05:12:11 +0100

    add openapi-swagger setup

--- src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
+++ src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.altice.alticcisequence.dto.AlticciSequenceDto;
 import com.altice.alticcisequence.service.AlticciSequenceService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Api(tags="Sequence values")
 @RequestMapping("/alticci")
 public class AlticciSequenceController {
 
@@ -23,9 +30,10 @@ public class AlticciSequenceController {
 	@Autowired
 	private AlticciSequenceDto alticciSequenceDto;
 
+	@ApiOperation("Get Alticci sequence value by index")
 	@GetMapping("/{n}")
-	public ResponseEntity<Object> getAlticciSequenceValue(@PathVariable Long n) {
-
+	public ResponseEntity<Object> getAlticciSequenceValue(@ApiParam(value="Required sequence index") @PathVariable Long n) {
+		
 		if(this.alticciSequenceDto.isAlticciSequenceIndexValid(n)){
 			this.alticciSequenceDto.setAlticciSequenceIndex(n);
 			return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS)).body(
--- src/main/java/com/altice/alticcisequence/openapi/SpringFoxConfig.java
+++ src/main/java/com/altice/alticcisequence/openapi/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.altice.alticcisequence.openapi;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,7 +17,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SpringFoxConfig implements WebMvcConfigurer {
 
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.altice.alticcisequence.controller")).paths(PathSelectors.any())
+				.build().apiInfo(apiInfo()).tags(new Tag("Sequence value", "Manage Alticci sequence values."));
+	}
+
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Alticci Sequence API").description("API for Altice Labs").version("1")
+				.build();
 	}
 }
